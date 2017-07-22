3b632ca5a3ebb2b6a052f7e07b2919fc3e4bb6f8
Johan le Roux <jleroux99@gmail.com>
2017-07-22 12:19:52 +0200

    Add Delete Functionality to CRUD

--- resources/views/customer/edit.blade.php
+++ resources/views/customer/edit.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.backend')
 @section('content-header')
     {!! Breadcrumbs::render('customer_edit', $customer) !!}
+
+    <div class="btn-group pull-right">
+        <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown">Actions <i class="fa fa-angle-down"></i></button>
+        <ul class="dropdown-menu pull-right" role="menu">
+            <li><a href="{{ action('CustomersController@show', $customer) }}"><i class="fa fa-eye"></i> View</a></li>
+            <li><a href="#" onclick="$('#customer_destroy').submit();"><i class="fa fa-trash"></i> Archive</a></li>
+            {{ html()->form('DELETE', action('CustomersController@destroy', $customer))->id('customer_destroy')->open() }}
+            {{ html()->form()->close() }}
+        </ul>
+    </div>
 @endsection
 
 @section('content')
