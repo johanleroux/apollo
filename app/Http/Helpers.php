7bd1d29eb0ac35008a3fdcf52882198ab5ef38ff
Johan le Roux <jleroux99@gmail.com>
2017-08-03 22:18:06 +0200

    Add Array Helper

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -4,3 +4,18 @@ function price_format($value)
 {
     return 'R ' . number_format($value, 2, '.', ' ');
 }
+
+function partition(array $list, $p)
+{
+    $listlen = count($list);
+    $partlen = floor($listlen / $p);
+    $partrem = $listlen % $p;
+    $partition = array();
+    $mark = 0;
+    for ($px = 0; $px < $p; $px ++) {
+        $incr = ($px < $partrem) ? $partlen + 1 : $partlen;
+        $partition[$px] = array_slice($list, $mark, $incr);
+        $mark += $incr;
+    }
+    return $partition;
+}
