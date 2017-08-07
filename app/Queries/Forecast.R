5ed11a4a84120fd435f26b738cf96b59fa0ef4ed
Ryan Henstock <ryan@pctec.co.za>
2017-08-07 17:52:48 +0200

    Added Forecast.R
    The forecasting function used by the system to extract data from text files and produce forecasted data ready to plotted onto a graph.

---
+++ app/Queries/Forecast.R
@@ -0,0 +1,24 @@
+list.of.packages <- c("ggplot2", "forecast", "reshape2")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos='http://cran.us.r-project.org', dependencies=TRUE)
+
+library(forecast)
+library(ggplot2)
+library(reshape2)
+
+getScriptPath <- function(){
+    cmd.args <- commandArgs()
+    m <- regexpr("(?<=^--file=).+", cmd.args, perl=TRUE)
+    script.dir <- dirname(regmatches(cmd.args, m))
+    if(length(script.dir) == 0) stop("can't determine script dir: please call the script with Rscript")
+    if(length(script.dir) > 1) stop("can't determine script dir: more than one '--file' argument detected")
+    return(script.dir)
+}
+
+setwd(getScriptPath())
+
+Frame <- read.table("data/input.txt", stringsAsFactors=FALSE, header=TRUE)
+Frame$Time <- as.Date(Frame$Time, format= "%Y-%m-%d")
+Frame <- ts(Frame$Pricing, start = 1, end=NROW(Frame), frequency=1)
+TheForecast <- naive(Frame)
+write.table(TheForecast,file="data/output.txt",quote=F)
