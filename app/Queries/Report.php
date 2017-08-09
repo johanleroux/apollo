8edca071230811342a63418f4a1e08a96fcd56c7
Johan le Roux <jleroux99@gmail.com>
2017-08-09 11:56:04 +0200

    Add Products Reporting

--- app/Queries/Report.php
+++ app/Queries/Report.php
@@ -12,14 +12,23 @@ use App\Models\PurchaseItem;
 class Report
 {
     protected $stockData;
+    protected $endDate;
+    protected $startDate;
 
-    public function __construct()
+    public function __construct($preload = true)
     {
-        $this->stockData = Product::with([
-            'purchaseItems',
-            'saleItems'
-        ])
-        ->get();
+        if ($preload) {
+            $this->stockData = Product::with([
+                'purchaseItems',
+                'saleItems'
+            ])
+            ->get();
+        }
+
+        $this->endDate   = new \Carbon\Carbon('last day of last month');
+        $this->endDate   = $this->endDate->endOfDay();
+        $this->startDate = new \Carbon\Carbon('last day of last month');
+        $this->startDate = $this->startDate->subMonths(11)->startOfMonth()->startOfDay();
     }
 
     public function stockUnits()
@@ -36,6 +45,54 @@ class Report
         });
     }
 
+    public function topProductByValue()
+    {
+        return SaleItem::select('product_id', DB::raw('SUM(price * quantity) value'))
+                ->whereBetween('created_at', [$this->startDate, $this->endDate])
+                ->groupBy('product_id')
+                ->orderBy('value', 'desc')
+                ->with('product')
+                ->first();
+    }
+
+    public function topProductByQuantity()
+    {
+        return SaleItem::select('product_id', DB::raw('SUM(quantity) quantity'))
+                ->whereBetween('created_at', [$this->startDate, $this->endDate])
+                ->groupBy('product_id')
+                ->orderBy('quantity', 'desc')
+                ->with('product')
+                ->first();
+    }
+
+    public function topSellerOfProduct()
+    {
+        return SaleItem::select('product_id', DB::raw('SUM(price * quantity) value'))
+                ->whereBetween('created_at', [$this->startDate, $this->endDate])
+                ->groupBy('product_id')
+                ->orderBy('value', 'desc')
+                ->with('product')
+                ->first();
+    }
+
+    public function lastSalesOfProduct($product_id, $limit = 5)
+    {
+        return SaleItem::where('product_id', $product_id)
+                        ->with('sale.customer')
+                        ->orderBy('created_at', 'desc')
+                        ->distinct('sale_id')
+                        ->limit($limit)
+                        ->get();
+    }
+
+    public function unitsInStock($product_id)
+    {
+        return (int) PurchaseItem::where('product_id', $product_id)
+                                 ->sum('quantity')
+             - (int) SaleItem::where('product_id', $product_id)
+                             ->sum('quantity');
+    }
+
     public function estimateMargin()
     {
         return $this->stockData->sum(function ($product) {
@@ -43,28 +100,26 @@ class Report
         });
     }
 
-    public function yearlyRecap()
+    public function yearlyRecap($limitToProduct = null)
     {
-        $endDate   = new \Carbon\Carbon('last day of last month');
-        $endDate   = $endDate->endOfDay();
-        $startDate = new \Carbon\Carbon('last day of last month');
-        $startDate = $startDate->subMonths(11)->startOfMonth()->startOfDay();
+        $data['startDate'] = $this->startDate->format('j M, Y');
+        $data['endDate']   = $this->endDate->format('j M, Y');
 
-        $data['startDate'] = $startDate->format('j M, Y');
-        $data['endDate']   = $endDate->format('j M, Y');
 
         $monthlySales =  SaleItem::select(DB::raw('SUM(price * quantity) value'), DB::raw('YEAR(created_at) year, MONTH(created_at) month'))
                 ->groupBy('year', 'month')
-                ->whereBetween('created_at', [$startDate, $endDate])
-                ->get();
+                ->whereBetween('created_at', [$this->startDate, $this->endDate]);
 
-        foreach ($monthlySales as $month) {
-            $data['labels'][] = '"'.(string) date('M', mktime(0, 0, 0, $month->month, 10)).'"';
-            $data['data'][]   = $month->value;
+        if ($limitToProduct) {
+            $monthlySales->where('product_id', $limitToProduct);
         }
 
-        $data['data']   = implode(',', $data['data']);
-        $data['labels'] = implode(',', $data['labels']);
+        $monthlySales = $monthlySales->get();
+
+        foreach ($monthlySales as $month) {
+            $data['labels'][] = date('M', mktime(0, 0, 0, $month->month, 10));
+            $data['data'][]   = $month->value;
+        }
 
         return $data;
     }
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,25 +1,51 @@
 @extends('layouts.backend')
 
 @section('content-header')
-  {!! Breadcrumbs::render('product') !!}
+    {!! Breadcrumbs::render('product') !!}
 
-  <div class="btn-group pull-right">
-    <a href="{{ action('ProductsController@create') }}" class="btn btn-sm"> Create <i class="fa fa-plus"></i></a>
-  </div>
+    <div class="btn-group pull-right">
+        <a href="{{ action('ProductsController@create') }}" class="btn btn-sm"> Create <i class="fa fa-plus"></i></a>
+    </div>
 @endsection
 
 @section('content')
-  <div class="box box-default">
-    <div class="box-body ">
-      {!! $dataTable->table(['class' => 'table table-striped table-hover table-responsive', 'width' => '100%']) !!}
+    <div class="row">
+        <div class="col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="ion ion-ios-cart-outline"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Top By Quantity</span>
+                    <span class="info-box-number">
+                        <a class="text-black" href="{{ action('ProductsController@show', $report->topProductByQuantity()->product) }}">{{ $report->topProductByQuantity()->product->sku }}</a><br>
+                        <small>{{ $report->topProductByQuantity()->quantity }} units</small>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-green"><i class="fa fa-money"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Top By Value</span>
+                    <span class="info-box-number">
+                        <a class="text-black" href="{{ action('ProductsController@show', $report->topProductByValue()->product) }}">{{ $report->topProductByValue()->product->sku }}</a><br>
+                        <small>{{ price_format($report->topProductByValue()->value) }}</small>
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+    <div class="box box-default">
+        <div class="box-body ">
+            {!! $dataTable->table(['class' => 'table table-striped table-hover table-responsive', 'width' => '100%']) !!}
+        </div>
+    </div>
 @endsection
 
 @push('js-after')
-  <script type="text/javascript">
-  $(document).ready(function () {
-    {{ $dataTable->generateScripts() }}
-  });
-  </script>
+    <script type="text/javascript">
+    $(document).ready(function () {
+        {{ $dataTable->generateScripts() }}
+    });
+</script>
 @endpush
