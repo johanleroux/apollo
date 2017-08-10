13a72a5a4a891b110fe69d278681290ea1637d79
Ryan Henstock <ryan@pctec.co.za>
2017-08-10 11:45:05 +0200

    Changed from naive model to Arima model

--- app/Queries/Forecast.R
+++ app/Queries/Forecast.R
@@ -20,5 +20,10 @@ setwd(getScriptPath())
 Frame <- read.table("data/input.txt", stringsAsFactors=FALSE, header=TRUE)
 Frame$Time <- as.Date(Frame$Time, format= "%Y-%m")
 Frame <- ts(Frame$Value, start = 1, end=NROW(Frame), frequency=1)
-TheForecast <- naive(Frame)
-write.table(TheForecast,file="data/output.txt",quote=F)
+
+TheForecast <- arima(Frame, order = c(10,1,0), method="ML")
+
+MyForecast <- plot(forecast(TheForecast,h=10))
+
+write.table(MyForecast,file="data/output.txt",quote=F)
+
