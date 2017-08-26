3b33fd7d11c5140c47096c3a6347d99329b48b4f
Johan le Roux <jleroux99@gmail.com>
2017-08-26 21:31:30 +0200

    Add DB Snapshots

--- database/seeds/CustomersSeeder.php
+++ database/seeds/CustomersSeeder.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Seeder;
 
 class CustomersSeeder extends Seeder
 {
+    protected $nCustomers = 50;
+    protected $nSales     = 50;
+    protected $nSaleItems = 5;
+
     /**
     * Run the database seeds.
     *
@@ -18,36 +22,67 @@ class CustomersSeeder extends Seeder
         Customer::flushEventListeners();
         Sale::flushEventListeners();
 
-        $products = Product::get();
-
         // Generate Customers
-        factory(Customer::class, 50)
+        factory(Customer::class, $this->nCustomers)
         ->create()
-        ->each(function ($c) use ($products) {
-            // Generate Sales
-            for ($x = 0; $x < 5; $x++) {
-                $date = new Carbon\Carbon;
-                $date = $date->subMonths(12);
-                $date->addWeeks(rand(1, 52))->format('Y-m-d H:i:s');
+        ->each(function ($c) {
+            // Generate Sales For Customers
+            $this->generateSales($c, $this->nSales);
 
-                $c->sales()->save(factory(Sale::class)->make([
-                    'created_at' => $date,
-                    'updated_at' => $date
-                ]));
-            }
-
-            $c->sales()->each(function ($s) use ($products) {
-                for ($x = 0; $x < 3; $x++) {
-                    $product = $products->random();
-
-                    $s->sale_items()->save(factory(SaleItem::class)->make([
-                        'product_id' => $product->id,
-                        'price'      => $product->recommended_selling_price,
-                        'created_at' => $s->created_at,
-                        'updated_at' => $s->updated_at
-                    ]));
-                }
-            });
+            // Generate Sales Items For Customers
+            $this->generateSaleItems($c, $this->nSaleItems);
         });
+    }
+
+    protected function generateSales($customer, $amount = 50)
+    {
+        for ($x = 0; $x < $amount; $x++) {
+            $date = new Carbon\Carbon;
+            $date = $date->subMonths(36);
+            $date->addWeeks(rand(1, 52*3))->format('Y-m-d H:i:s');
+
+            $customer
+                ->sales()
+                ->save(
+                    factory(Sale::class)->make([
+                        'customer_id'  => $customer,
+                        'created_at'   => $date,
+                        'updated_at'   => $date
+                    ])
+                );
+        }
+    }
+
+    protected function generateSaleItems($customer, $amount = 50)
+    {
+        $customer
+            ->sales
+            ->each(function ($sale) use ($customer, $amount) {
+                    for($y = 0; $y < $this->nSaleItems; $y++) {
+                        $product = Product::inRandomOrder()
+                                    ->with(['closed_purchase_items', 'sale_items'])
+                                    ->first();
+
+                        $stock = (int) $product->stockQuantity;
+                        $stock = $stock < 1000 ?: 1000;
+
+                        if($stock >  0)
+                        {
+                            // Create Sale Item
+                            $sale
+                                ->sale_items()
+                                ->save(
+                                    factory(SaleItem::class)->make([
+                                        'sale_id'     => $sale->id,
+                                        'product_id'  => $product->id,
+                                        'price'       => $product->recommended_selling_price,
+                                        'quantity'    => rand(1, $stock),
+                                        'created_at'  => $sale->created_at,
+                                        'updated_at'  => $sale->created_at
+                                    ])
+                                );
+                        }
+                    }
+            });
     }
 }
