fd6e9dacc9bccce7f4c0077df1fbe8ce306789e1
Johan le Roux <jleroux99@gmail.com>
2017-09-03 19:42:55 +0200

    Add Laravel Horizon

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,8 +2,9 @@
 
 @task('deploy')
     cd /home/forge/apollo.lomejo.co.za
+    php artisan horizon:terminate
     git pull https://johanleroux:P1XKQf80xd8wPJAExpUv7sVX5jYC1DfI16a1VthekxB35ep43EU6VgjH2uskXpnS@github.com/IFMTYP/team19.git master
     composer install --no-interaction --prefer-dist --optimize-autoloader
-    php artisan queue:restart
+    php artisan horizon
     php artisan snapshot:load db_forecasted
 @endtask
