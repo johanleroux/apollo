6b58d9de50e81e6ec66efa2365ecf48b3cb7ab4d
Johan le Roux <jleroux99@gmail.com>
2017-09-03 19:58:43 +0200

    Update Envoy Deployment

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -5,6 +5,5 @@
     php artisan horizon:terminate
     git pull https://johanleroux:P1XKQf80xd8wPJAExpUv7sVX5jYC1DfI16a1VthekxB35ep43EU6VgjH2uskXpnS@github.com/IFMTYP/team19.git master
     composer install --no-interaction --prefer-dist --optimize-autoloader
-    php artisan horizon
     php artisan snapshot:load db_forecasted
 @endtask
