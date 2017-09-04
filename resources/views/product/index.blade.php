1b17a6e76e1e9a7bc34cd21e33a121be38b3e0f6
Johan le Roux <jleroux99@gmail.com>
2017-09-04 15:58:44 +0200

    Add Heading for Resources

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.backend')
-
-@section('content-header')
+    @section('content-header')
     {!! Breadcrumbs::render('product') !!}
-
-    <div class="btn-group pull-right">
-    </div>
 
     <div class="btn-group pull-right">
         <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown">Actions <i class="fa fa-angle-down"></i></button>
@@ -18,39 +14,46 @@
             <li><a target="_blank" href="{{ action('CsvController@purchases') }}"><i class="fa fa-external-link"></i> Purchases</a></li>
             <li><a target="_blank" href="{{ action('CsvController@open_purchases') }}"><i class="fa fa-external-link"></i> Open Purchases</a></li>
         </ul>
-    </div>    
-@endsection
-
+    </div>
+    @endsection
 @section('content')
     @if(!request()->has('supplier_id'))
-        <div class="row">
-            <div class="col-sm-6 col-xs-12">
-                <div class="info-box">
-                    <span class="info-box-icon bg-aqua"><i class="ion ion-ios-cart-outline"></i></span>
-                    <div class="info-box-content">
-                        <span class="info-box-text">Top By Quantity</span>
-                        <span class="info-box-number">
-                            <a class="text-black" href="{{ action('ProductsController@show', $stats['quantity']['product']) }}">{{ $stats['quantity']['product']->sku }}</a><br>
-                            <small>{{ $stats['quantity']['quantity'] }} units</small>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-xs-12">
-                <div class="info-box">
-                    <span class="info-box-icon bg-green"><i class="fa fa-money"></i></span>
-                    <div class="info-box-content">
-                        <span class="info-box-text">Top By Value</span>
-                        <span class="info-box-number">
-                            <a class="text-black" href="{{ action('ProductsController@show', $stats['value']['product']) }}">{{ $stats['value']['product']->sku }}</a><br>
-                            <small>{{ price_format($stats['value']['value']) }}</small>
-                        </span>
-                    </div>
+    <div class="row">
+        <div class="col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="ion ion-ios-cart-outline"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Top By Quantity</span>
+                    <span class="info-box-number">
+                        <a class="text-black" href="{{ action('ProductsController@show', $stats['quantity']['product']) }}">{{ $stats['quantity']['product']->sku }}</a><br>
+                        <small>{{ $stats['quantity']['quantity'] }} units</small>
+                    </span>
                 </div>
             </div>
         </div>
+        <div class="col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-green"><i class="fa fa-money"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Top By Value</span>
+                    <span class="info-box-number">
+                        <a class="text-black" href="{{ action('ProductsController@show', $stats['value']['product']) }}">{{ $stats['value']['product']->sku }}</a><br>
+                        <small>{{ price_format($stats['value']['value']) }}</small>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
     @endif
     <div class="box box-default">
+        @if(request()->has('supplier_id'))
+            <div class="box-header with-border">
+                    @php 
+                        $supplier = \App\Models\Supplier::find(request()->supplier_id);
+                    @endphp
+                <h3 class="box-title">Supplier - <a href="{{ action('SuppliersController@show', $supplier) }}">{{ $supplier->name}}</a></h3>
+            </div>
+        @endif
         <div class="box-body ">
             {!! $dataTable->table(['class' => 'table table-striped table-hover table-responsive', 'width' => '100%']) !!}
         </div>
@@ -59,8 +62,8 @@
 
 @push('js-after')
     <script type="text/javascript">
-    $(document).ready(function () {
-        {{ $dataTable->generateScripts() }}
-    });
-</script>
+        $(document).ready(function () {
+            {{ $dataTable->generateScripts() }}
+        });
+    </script>
 @endpush
--- resources/views/purchase/index.blade.php
+++ resources/views/purchase/index.blade.php
@@ -10,13 +10,22 @@
 
 @section('content')
   <div class="box box-default">
+    @if(request()->has('supplier_id'))
+      <div class="box-header with-border">
+        @php 
+          $supplier = \App\Models\Supplier::find(request()->supplier_id);
+        @endphp
+        <h3 class="box-title">Supplier - <a href="{{ action('SuppliersController@show', $supplier) }}">{{ $supplier->name}}</a></h3>
+      </div>
+    @elseif(request()->has('product_id'))
+      <div class="box-header with-border">
+        @php 
+          $product = \App\Models\Product::find(request()->product_id);
+        @endphp
+        <h3 class="box-title">Product - <a href="{{ action('ProductsController@show', $product) }}">{{ $product->sku}}</a></h3>
+      </div>
+    @endif
     <div class="box-body ">
-      @if(request()->has('supplier_id'))
-        Supplier: #{{ request()->supplier_id }} <br>
-      @endif
-      @if(request()->has('product_id'))
-        Product: #{{ request()->product_id }} <br>
-      @endif
       {!! $dataTable->table(['class' => 'table table-striped table-hover table-responsive', 'width' => '100%']) !!}
     </div>
   </div>
